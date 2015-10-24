8d206ebd7ebe23e0d40900dbfbf1fd8a859f7f02
arcangel666 <arcangel666@hotmail.co.uk>
2015-10-24 12:38:25 +0100

    Added CompetitionMatchTypeView and interface.

---
+++ GAA_Paper_Templates/GAA_Paper_Templates/Interfaces/ICompetitionMatchTypeView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace GAA_Paper_Templates
+{
+    interface ICompetitionMatchTypeView
+    {
+        CompetitionMatchType CreateCompetitionMatchType(string name);
+        CompetitionMatchType DeleteCompetitionMatchType(CompetitionMatchType matchType);
+        List<CompetitionMatchType> GetAllCompetitionMatchTypes();
+        List<CompetitionMatchType> GetCompetitionMatchTypesByCompetition(Competition comp);
+        CompetitionMatchType UpdateCompetitionMatchType(CompetitionMatchType matchType, string name);
+    }
+}
---
+++ GAA_Paper_Templates/GAA_Paper_Templates/Views/CompetitionMatchTypeView.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAA_Paper_Templates.Views
+{
+    class CompetitionMatchTypeView : ICompetitionMatchTypeView
+    {
+        GAA_Templates_ModelContainer context;
+        public CompetitionMatchTypeView(GAA_Templates_ModelContainer _context)
+        {
+            context = _context;
+        }
+
+        public CompetitionMatchType CreateCompetitionMatchType(string name)
+        {
+            CompetitionMatchType c = new CompetitionMatchType();
+
+            c.Name = name;
+
+            context.CompetitionMatchTypes.Add(c);
+
+            context.SaveChanges();
+
+            return c;
+        }
+
+        public CompetitionMatchType UpdateCompetitionMatchType(CompetitionMatchType matchType, string name)
+        {
+            CompetitionMatchType cmt = context.CompetitionMatchTypes
+                .Where(c => c == matchType)
+                .FirstOrDefault();
+
+            if (cmt != null)
+            {
+                cmt.Name = name;
+
+                context.SaveChanges();
+
+                return cmt; 
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public CompetitionMatchType DeleteCompetitionMatchType(CompetitionMatchType matchType)
+        {
+            CompetitionMatchType cmt = context.CompetitionMatchTypes
+                .Where(c => c == matchType)
+                .FirstOrDefault();
+
+            if (cmt != null)
+            {
+                context.CompetitionMatchTypes.Remove(cmt);
+                context.SaveChanges();
+
+                return cmt;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<CompetitionMatchType> GetAllCompetitionMatchTypes()
+        {
+            return context.CompetitionMatchTypes.ToList();
+        }
+
+        public List<CompetitionMatchType> GetCompetitionMatchTypesByCompetition(Competition comp)
+        {
+            return context.CompetitionMatchTypes
+                .Where(c => c.Competitions == comp)
+                .Distinct()
+                .ToList();
+        }
+    }
+}
