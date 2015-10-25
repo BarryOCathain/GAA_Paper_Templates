d16d89237c00d3c5972a3e6227e4c1965f7b10e9
arcangel666 <arcangel666@hotmail.co.uk>
2015-10-25 19:36:06 +0000

    Corrected untracked files.

---
+++ GAA_Paper_Templates/Client_Layer/CountyWindow.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Client_Layer
+{
+    partial class CountyWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.provinceComboBox = new System.Windows.Forms.ComboBox();
+            this.provinceLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.countyLabel = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // provinceComboBox
+            // 
+            this.provinceComboBox.FormattingEnabled = true;
+            this.provinceComboBox.Location = new System.Drawing.Point(73, 32);
+            this.provinceComboBox.Name = "provinceComboBox";
+            this.provinceComboBox.Size = new System.Drawing.Size(121, 21);
+            this.provinceComboBox.TabIndex = 0;
+            // 
+            // provinceLabel
+            // 
+            this.provinceLabel.AutoSize = true;
+            this.provinceLabel.Location = new System.Drawing.Point(12, 35);
+            this.provinceLabel.Name = "provinceLabel";
+            this.provinceLabel.Size = new System.Drawing.Size(55, 13);
+            this.provinceLabel.TabIndex = 1;
+            this.provinceLabel.Text = "Province :";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(73, 6);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(121, 20);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // countyLabel
+            // 
+            this.countyLabel.AutoSize = true;
+            this.countyLabel.Location = new System.Drawing.Point(12, 9);
+            this.countyLabel.Name = "countyLabel";
+            this.countyLabel.Size = new System.Drawing.Size(46, 13);
+            this.countyLabel.TabIndex = 3;
+            this.countyLabel.Text = "County :";
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(118, 60);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 4;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(12, 60);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // CountyWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(209, 93);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.countyLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.provinceLabel);
+            this.Controls.Add(this.provinceComboBox);
+            this.Name = "CountyWindow";
+            this.Text = "County";
+            this.Load += new System.EventHandler(this.CountyWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox provinceComboBox;
+        private System.Windows.Forms.Label provinceLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label countyLabel;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
---
+++ GAA_Paper_Templates/Client_Layer/CountyWindow.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GAA_Paper_Templates;
+using GAA_Paper_Templates.Common;
+using GAA_Paper_Templates.Interfaces;
+using GAA_Paper_Templates.Views;
+
+namespace Client_Layer
+{
+    public partial class CountyWindow : Form, ICountyView
+    {
+        private GAA_Templates_ModelContainer context;
+        private CountyView countyViewContext;
+
+        private bool update;
+        public CountyWindow(bool _update = false)
+        {
+            InitializeComponent();
+
+            update = _update;
+
+            context = new GAA_Templates_ModelContainer();
+            countyViewContext = new CountyView(context);
+
+            bindProvincesToComboBox();
+        }
+
+        private void CountyWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindProvincesToComboBox()
+        {
+            this.provinceComboBox.DataSource = Enum.GetValues(typeof(Enums.Provinces));
+        }
+
+        #region CountyView Methods
+        public County CreateCounty(string name, Enums.Provinces province)
+        {
+            using (countyViewContext)
+            {
+                return countyViewContext.CreateCounty(name, province);
+            }
+        }
+
+        public County DeleteCounty(County county)
+        {
+            throw new NotImplementedException();
+        }
+
+        public County GetCounty(string name)
+        {
+            using (countyViewContext)
+            {
+                return countyViewContext.GetCounty(name);
+            }
+        }
+
+        public List<County> GetAllCounties()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<County> GetCountiesByProvince(Enums.Provinces province)
+        {
+            throw new NotImplementedException();
+        }
+
+        public County UpdateCounty(County county, string name, Enums.Provinces province)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(this.nameTextBox.Text))
+            {
+                County cty = GetCounty(this.nameTextBox.Text);
+                if (update && cty != null)
+                {
+                    UpdateCounty(GetCounty(this.nameTextBox.Text), this.nameTextBox.Text, (Enums.Provinces)this.provinceComboBox.SelectedItem);
+                    MessageBox.Show("County Updated.");
+                }
+                else if (!update && cty == null)
+                {
+                    CreateCounty(this.nameTextBox.Text, (Enums.Provinces)this.provinceComboBox.SelectedItem);
+                }
+
+                this.DialogResult = DialogResult.OK;
+            }
+        }
+    }
+}
